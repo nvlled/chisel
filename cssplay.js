89fbe65cc048976533a7ab9aea6b670fc0194f37
ron <nvlled@gmail.com>
2015-10-03 22:05:10 +0800

    Initial commit

---
+++ cssplay.js
@@ -0,0 +1,223 @@
+// TODO: provide navigation for all levels (an index)
+// TODO: have sympathy for the color blinds
+
+function copyChildren(dest, src) {
+    var frag = document.createDocumentFragment();
+    src = src.cloneNode(true);
+    while (src.firstChild) {
+        frag.appendChild(src.firstChild);
+    }
+    dest.appendChild(frag);
+}
+
+function selector(node, s, prop) {
+    var subNode = node.querySelector(s);
+    if (subNode && prop)
+        return subNode[prop];
+    return subNode;
+}
+
+// ----------------------------------------------------------
+
+function getLevelData(node) {
+    return {
+        title: selector(node, ".title", "textContent"),
+        info: selector(node, ".info", "innerHTML"),
+        goal: selector(node, ".goal", "textContent"),
+        node: selector(node, ".contents"),
+    }
+}
+
+function setLevel(mainNode, levelNode) {
+    var levelData = getLevelData(levelNode);
+    mainNode.querySelector(".title").textContent = levelData.title;
+    mainNode.querySelector(".info").innerHTML = levelData.info;
+    mainNode.querySelector(".goal").textContent = levelData.goal;
+    var contentsNode = mainNode.querySelector(".contents");
+    contentsNode.innerHTML = "";
+    copyChildren(contentsNode, levelData.node);
+}
+
+// ----------------------------------------------------------
+
+var store = {
+    namespace: "localStorage.cssplay.",
+    set: function(key, val) {
+        localStorage[this.namespace+key] = val;
+    },
+    get: function(key) {
+        return localStorage[this.namespace+key];
+    },
+    setJSON: function(key, obj) {
+        this.set(key, JSON.stringify(obj));
+    },
+    getJSON: function(key) {
+        try {
+            return JSON.parse(this.get(key));
+        } catch(e) {
+            return null;
+        }
+    }
+}
+
+// ----------------------------------------------------------
+
+var state = {
+    level: 0,
+    //savedSelectors: store.getJSON("savedSelectors"),
+    completedLevels: [true, false, true],
+}
+
+var main = document.querySelector("#main");
+var input = document.querySelector("input");
+var errmsg = document.querySelector(".errmsg");
+var completed = document.querySelector(".completed");
+var levels = document.querySelector("#levels");
+
+levels.remove();
+
+var CORRECT_CLASS = "_X_";
+var CHEATMSG = "NOCHEAT";
+
+function clearSelected() {
+    var nodes = document.querySelectorAll(".selected");
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].classList.remove("selected");
+    }
+    completed.style.display = "none";
+}
+
+function getTargetNodes() {
+    return main.querySelectorAll(".contents ." + CORRECT_CLASS);
+}
+
+function getSelected(sel) {
+    var result = [];
+    if (!sel)
+        return result;
+    sel.split(",").forEach(function(val) {
+        // , aren't actually combinators?
+        var sel = "#main .contents " + val.trim();
+        sel = sel.replace(new RegExp(CORRECT_CLASS), CHEATMSG);
+        var nodes = document.querySelectorAll(sel);
+        result.concat(nodes);
+        for (var i = 0; i < nodes.length; i++)
+            result.push(nodes[i]);
+    });
+    return result;
+}
+
+function checkAnswer() {
+    try {
+        var nodes = getSelected(input.value);
+        var targetNodes = getTargetNodes();
+
+        if (nodes.length != targetNodes.length)
+            return;
+
+        for (var i = 0; i < nodes.length; i++) {
+            var ok = false;
+            for (var j = 0; j < targetNodes.length; j++) {
+                if (nodes[i] == targetNodes[j]) {
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok)
+                return;
+        }
+        console.log("correct ✓");
+        document.querySelector("button.next").focus();
+        completed.style.display = "inherit";
+    } catch (e) { }
+}
+
+function showSelected() {
+    errmsg.textContent = "";
+    clearSelected();
+
+    if (!input.value) {
+        if (!input.value)
+            return;
+    }
+    // TODO: lookup how to detect document reloading
+
+    var noneFound = true;
+    var err;
+
+    try {
+        var nodes = getSelected(input.value);
+        if (nodes.length > 0) {
+            for (var i = 0; i < nodes.length; i++) {
+                nodes[i].classList.add("selected");
+            }
+        } else {
+            errmsg.textContent = "no element found";
+        }
+    } catch (e) {
+        errmsg.textContent = "invalid syntax";
+    }
+}
+
+function setLevelNo(n) {
+    var level = levels.children[n];
+    if (level) {
+        store.set("level", n);
+        setLevel(main, level);
+
+        if (state.completedLevels[n]) {
+            completed.style.display = "inherit";
+        } else {
+            completed.style.display = "none";
+        }
+
+        var val = store.get("savedSelector."+state.level);
+        input.value = val || "";
+        showSelected();
+        checkAnswer();
+    } else {
+        console.log("invalid level number: ", n);
+    }
+}
+
+input.addEventListener("keypress", function(e) {
+    if (e.keyCode == 13) {
+        showSelected();
+        checkAnswer();
+        store.set("savedSelector."+state.level, input.value);
+    } else if (e.keyCode == 27) {
+        document.querySelector("button.prev").focus();
+    }
+});
+
+function prevLevel() {
+    if (state.level > 0) {
+        state.level--;
+        setLevelNo(state.level);
+    }
+}
+
+function nextLevel() {
+    if (levels.children[state.level+1]) {
+        state.level++
+        setLevelNo(state.level);
+    }
+}
+
+main.querySelector("button.prev").onclick = prevLevel;
+main.querySelector("button.next").onclick = nextLevel;
+
+document.addEventListener("keypress", function(e) {
+    if (e.target == input)
+        return;
+
+    if (e.key == 'f')
+        input.focus();
+    else if (e.key == 'p')
+        prevLevel();
+    else if (e.key == 'n')
+        nextLevel();
+})
+
+state.level = +store.get("level") || 0;
+setLevelNo(state.level);
